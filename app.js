10861807d08d4c85e67587a0bac0db97934e597e
ATISH YADAV <atishcomp@gmail.com>
2022-09-24 21:18:26 +0530

    hello

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express =require("express");
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose");
-var express =require("express");
-var cookieParser= require("cookie-parser");
+// const jwt = require("jsonwebtoken")
+// const mongoose = require("mongoose");
+// var express =require("express");
+// var cookieParser= require("cookie-parser");
 var cors = require("cors");
-var bodyParser= require("body-parser");
-const fileUpload= require("express-fileupload")
-const cloudinary = require('cloudinary').v2
+// var bodyParser= require("body-parser");
+// const fileUpload= require("express-fileupload")
+// const cloudinary = require('cloudinary').v2
 var app= express();
 app.use(cors())
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 // const userRoutes = require("./routes/user")
 // const imagedataRoutes = require("./routes/imagedata")
