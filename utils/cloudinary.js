2ab3923b1d09b5d463a45a11a7b3539e2342107c
Atish Yadav <88705566+atishcomp@users.noreply.github.com>
2023-05-25 22:32:49 +0530

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
     cloud_name:'deepotsav',
-    api_key:'367121351339583',
-    api_secret:'N95Ur9fwq_LmooptaRG0-n0XyRQ'
+    api_key:'367121351339452',
+    api_secret:'N95Ur9fwq_LmooptaRG0-nioPli'
 })
 
 module.exports = cloudinary;
